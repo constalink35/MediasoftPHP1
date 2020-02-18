8355b50849b522963824dede1253b3dbb66a26f0
constalink35 <constalink@gmail.com>
2020-02-18 16:23:09 +0400

    mod index

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+print <<<HEADER
+<!DOCTYPE  html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Академия разработки MediaSoft курс PHP</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+</head>
+<body>
+<div class="container">
+    <h1>Академия разработки MediaSoft курс PHP</h1>
+    <a href="ms_task1.php"><h3>Задание 1. Циклы и функции.</h3></a>
+HEADER;
+
+print <<<FOOTER
+</div>
+</body>
+</html>
+FOOTER;
+?>
