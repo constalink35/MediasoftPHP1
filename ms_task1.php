bf71f50f36ca725eed1a30148f6630486387edb1
constalink35 <constalink@gmail.com>
2020-02-18 16:15:41 +0400

    mod5

--- ms_task1.php
+++ ms_task1.php
@@ -36,6 +36,7 @@ print <<<HEADER
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Академия разработки MediaSoft курс PHP</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="icon" href="favicon.ico" type="image/x-icon"/>
 </head>
 <body>
 <div class="container">
